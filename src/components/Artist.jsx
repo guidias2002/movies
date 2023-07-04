68c5a311e9252efa9a92d6978743d606d11f0975
Guilherme Cordeiro Dias <guidias200220@gmail.com>
2023-07-04 17:29:53 -0300

    final project

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -8,25 +8,25 @@ import './Artist.css'
 
 export default function Artist() {
 
-    const [artists, setArtists] = useState([])
+    const [ artists, setArtists ] = useState([])
     const { id } = useParams()
 
     useEffect(() => {
         async function getArtists() {
             try {
-                const res = await fetch(
-                    `${api_artist}${id}/credits?${api_key}`
-                );
+                const res = await fetch(`${api_artist}${id}/credits?${api_key}`);
                 const data = await res.json();
                 const artists = data.cast;
-                const artistsMain = artists.slice(0,6)
-                setArtists(artistsMain)
-            } catch(error) {
-                console.log('Erro ao buscar os dados da API:', error);
+                const mainArtists = artists.slice(0, 6);
+
+                setArtists(mainArtists)
+            } catch (error) {
+                console.log('Erro ao buscar dados da API', error)
             }
         }
         getArtists()
     }, [])
+
 
     return (
         <ul className='listArtists'>
